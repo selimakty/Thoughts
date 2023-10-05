07a4d3338225f8048d646f947ce8ae437b77ddff
Selim Aktay <sellimakty99@gmail.com>
2023-10-05 14:33:30 +0300

    Initial Commit

---
+++ Thoughts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Thoughts
+//
+//  Created by Selim on 5.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
